707a866b4f85ae00b5476b7e8cce03ac7cf3476a
Dan Bright <dan@aninstanceofme.com>
2017-05-05 01:54:18 +0100

    minor version bump mirrored from dev repo

--- before-startup.sh
+++ before-startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## TO BE RUN WITHIN CONTAINER WHEN CONTAINER RUNNING!
-# update very latest django from from git to patch bugs etc
-/usr/bin/git -C /usr/local/lib/python3.5/dist-packages/django-git pull -a;
 # change git to lower version whilst template creation bug persists
 /usr/bin/git -C /usr/local/lib/python3.5/dist-packages/django-git checkout stable/1.10.x && \
+# update very latest django from from git to patch bugs etc
+/usr/bin/git -C /usr/local/lib/python3.5/dist-packages/django-git pull -a; && \
 rm -rf /usr/local/lib/python3.5/dist-packages/django && \
 cp -R /usr/local/lib/python3.5/dist-packages/django-git/django /usr/local/lib/python3.5/dist-packages/django;
 python3.5 /home/docker/code/manage.py makemigrations;
